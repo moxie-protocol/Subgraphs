7128fbe16d9cef8ed3e1c66b6092babe64637b4a
Hrishikesh K Thakkar <hrishikesh@airstack.xyz>
2024-07-22 15:05:31 +0400

    Revert "Initial Commit For transactional Changes"

--- moxie-auction-transactional/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const ORDER_STATUS_PLACED = "Placed"
-export const ORDER_STATUS_CLAIMED = "Claimed"
-export const ORDER_STATUS_CANCELLED = "Cancelled"
-export const ORDER_ENTITY_COUNTER_ID = "ORDER_ENTITY_COUNTER_ID"
