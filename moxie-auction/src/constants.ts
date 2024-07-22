60fab94eabe25953920b29c0aac33a8a2d98efe0
Hrishikesh Thakkar <kesh5210@gmail.com>
2024-07-22 10:56:56 +0400

    Schema changes for active Order Count and lastUpdatedIndex in orders

--- moxie-auction/src/constants.ts
+++ moxie-auction/src/constants.ts
@@ -1,3 +1,4 @@
 export const ORDER_STATUS_PLACED = "Placed"
 export const ORDER_STATUS_CLAIMED = "Claimed"
 export const ORDER_STATUS_CANCELLED = "Cancelled"
+export const ORDER_ENTITY_COUNTER_ID = "ORDER_ENTITY_COUNTER_ID"
