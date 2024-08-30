13bcc43686da19cf379fc803bfec386032574436
Sharath V <sharathkrml@gmail.com>
2024-08-30 18:27:12 +0530

    added router whitelisting

--- moxie-protocol-transactional/src/constants.ts
+++ moxie-protocol-transactional/src/constants.ts
@@ -9,3 +9,7 @@ BLACKLISTED_SUBJECT_TOKEN_ADDRESS.set("0x7412b5b7a7498f7b2a663b5c708a98f3092847f
 
 export const BLACKLISTED_AUCTION = new TypedMap<string, bool>()
 BLACKLISTED_AUCTION.set("228", true)
+
+export const WHITELISTED_ROUTERS = new TypedMap<string, bool>()
+// RainbowRouter
+WHITELISTED_ROUTERS.set("0x00000000009726632680fb29d3f7a9734e3010e2", true)
