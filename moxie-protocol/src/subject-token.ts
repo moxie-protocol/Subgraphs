3a0f579483ae217defcad98065c10f6bae19b7c2
Sharath V <sharathkrml@gmail.com>
2024-09-18 22:46:39 +0530

    fixed balance

--- moxie-protocol/src/subject-token.ts
+++ moxie-protocol/src/subject-token.ts
@@ -31,14 +31,12 @@ export function handleTransfer(event: Transfer): void {
   subjectToken.totalSupply = totalSupply
 
   // updating portfolios
-  // if (!mint && !isFromOrToStakingContract(from, to)) {
-  if (!mint ) {
+  if (!mint && !isFromOrToStakingContract(from, to)) {
     let fromAddressPortfolio = getOrCreatePortfolio(from, contractAddress, event.transaction.hash, event.block)
     fromAddressPortfolio.unstakedBalance = fromAddressPortfolio.unstakedBalance.minus(value)
     savePortfolio(fromAddressPortfolio, event.block)
   }
-  // if (!burn && !isFromOrToStakingContract(from, to)) {
-  if (!burn ) {
+  if (!burn && !isFromOrToStakingContract(from, to)) {
     let toAddressPortfolio = getOrCreatePortfolio(to, contractAddress, event.transaction.hash, event.block)
     // adding unique holders when a new portfolio is created
     if (toAddressPortfolio.unstakedBalance.equals(BigInt.fromI32(0))) {
