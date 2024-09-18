79498a45bcafe85d27dedda2caaaad9fb5d3f67e
Sharath V <sharathkrml@gmail.com>
2024-09-18 16:58:38 +0530

    added staking subgraph

---
+++ moxie-staking/src/staking.ts
@@ -0,0 +1,19 @@
+import { store } from "@graphprotocol/graph-ts"
+
+import { Lock } from "../generated/Staking/Staking"
+import { LockInfo } from "../generated/schema"
+import { getOrCreateBlockInfo } from "./utils"
+export function handleLock(event: Lock): void {
+  let lockInfo = new LockInfo(event.params._index.toString())
+  lockInfo.txHash = event.transaction.hash
+  lockInfo.logIndex = event.logIndex
+  lockInfo.user = event.params._user
+  lockInfo.subjectToken = event.params._subjectToken
+  lockInfo.subject = event.params._subject
+  lockInfo.unlockTimeInSec = event.params._unlockTimeInSec
+  lockInfo.amount = event.params._amount
+  lockInfo.isBuy = event.params._isBuy
+  lockInfo.lockPeriodInSec = event.params._lockPeriodInSec
+  lockInfo.createdAtBlockInfo = getOrCreateBlockInfo(event.block).id
+  lockInfo.save()
+}
