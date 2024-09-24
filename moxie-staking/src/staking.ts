1ef8477c5f2406a23a33c9aff94ca911f2d6ca2a
Sharath V <sharathkrml@gmail.com>
2024-09-24 22:53:55 +0530

    fixed subgraphs for new staking contracts

--- moxie-staking/src/staking.ts
+++ moxie-staking/src/staking.ts
@@ -1,4 +1,5 @@
 
+import { BigInt } from "@graphprotocol/graph-ts"
 import { Lock } from "../generated/Staking/Staking"
 import { LockInfo } from "../generated/schema"
 export function handleLock(event: Lock): void {
@@ -10,7 +11,7 @@ export function handleLock(event: Lock): void {
   lockInfo.subject = event.params._subject
   lockInfo.unlockTimeInSec = event.params._unlockTimeInSec
   lockInfo.amount = event.params._amount
-  lockInfo.isBuy = event.params._isBuy
+  lockInfo.isBuy = event.params._moxieDepositAmount.notEqual(BigInt.zero())
   lockInfo.lockPeriodInSec = event.params._lockPeriodInSec
   lockInfo.createdAtBlockNumber = event.block.number
   lockInfo.createdAtBlockTimestamp = event.block.timestamp
