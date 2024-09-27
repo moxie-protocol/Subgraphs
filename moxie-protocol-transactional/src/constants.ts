360fdacb448b9e32f6bfadcbfbf65ede5f2c4d62
Sharath V <sharathkrml@gmail.com>
2024-09-27 21:24:57 +0530

    added protocol transactional whitelisted

--- moxie-protocol-transactional/src/constants.ts
+++ moxie-protocol-transactional/src/constants.ts
@@ -6,6 +6,10 @@ export const PCT_BASE = BigInt.fromI32(10).pow(18)
 
 export const BLACKLISTED_SUBJECT_TOKEN_ADDRESS = new TypedMap<string, bool>()
 BLACKLISTED_SUBJECT_TOKEN_ADDRESS.set("0x7412b5b7a7498f7b2a663b5c708a98f3092847f8", true)
+BLACKLISTED_SUBJECT_TOKEN_ADDRESS.set(
+ "0x6dba346bb05e5644edb25f9b355b6110649178d1",
+ true
+)
 
 export const BLACKLISTED_AUCTION = new TypedMap<string, bool>()
 BLACKLISTED_AUCTION.set("228", true)
