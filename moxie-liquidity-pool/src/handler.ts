19b9a79843cfebef3d7a26839a2277c65e28ebe4
Sharath V <sharathkrml@gmail.com>
2025-01-10 20:08:20 +0530

    added pool moxieReserve handling

--- moxie-liquidity-pool/src/handler.ts
+++ moxie-liquidity-pool/src/handler.ts
@@ -11,7 +11,7 @@ import {
   Withdraw
 } from "../generated/AerodromeGauge/AerodromeGauge"
 import { Deposit as CLDeposit, Withdraw as CLWithdraw } from "../generated/AerodromeCLGauge/AerodromeCLGauge"
-import { Mint, Burn, SetGaugeAndPositionManagerCall } from "../generated/AerodromeCLPool/AerodromeCLPool"
+import { Mint, Burn, SetGaugeAndPositionManagerCall, Swap, Collect, CollectFees } from "../generated/AerodromeCLPool/AerodromeCLPool"
 import { getOrCreateBlockInfo, getOrCreatePoolEntity, getOrCreateUserEntity, getOrCreateUserPoolEntity, getV3NftIdentifier, handleSyncEvents, handleTransferEvents } from "./utils"
 import { GAUGE_LP_TOKEN_MAP } from "./constants"
 import { Address, BigInt, log } from "@graphprotocol/graph-ts"
@@ -45,11 +45,49 @@ export function handleMint(event: Mint): void {
   txEntity.save()
 
   handleTransferEvents(event, Address.zero(), event.transaction.from, event.params.amount, event.address.toHexString())
+  // handling pool
+  // amount0 is non moxie (for current case), may need to extend in the future
+  // amount1 is moxie (for current case), may need to extend in the future
+  let pool = getOrCreatePoolEntity(event, event.address.toHexString())
+  pool.nonMoxieReserve = pool.nonMoxieReserve.plus(event.params.amount0)
+  pool.moxieReserve = pool.moxieReserve.plus(event.params.amount1)
+  pool.updatedAt = getOrCreateBlockInfo(event).id
+  pool.save()
+}
+
+export function handleSwap(event: Swap): void {
+  // handling pool
+  // amount0 is non moxie (for current case), may need to extend in the future
+  // amount1 is moxie (for current case), may need to extend in the future
+  let pool = getOrCreatePoolEntity(event, event.address.toHexString())
+  pool.nonMoxieReserve = pool.nonMoxieReserve.plus(event.params.amount0)
+  pool.moxieReserve = pool.moxieReserve.plus(event.params.amount1)
+  pool.updatedAt = getOrCreateBlockInfo(event).id
+  pool.save()
+}
+
+export function handleCollectFees(event: CollectFees): void {
+  // handling pool
+  // amount0 is non moxie (for current case), may need to extend in the future
+  // amount1 is moxie (for current case), may need to extend in the future
+  let pool = getOrCreatePoolEntity(event, event.address.toHexString())
+  pool.nonMoxieReserve = pool.nonMoxieReserve.minus(event.params.amount0)
+  pool.moxieReserve = pool.moxieReserve.minus(event.params.amount1)
+  pool.updatedAt = getOrCreateBlockInfo(event).id
+  pool.save()
 }
 
 export function handleBurn(event: Burn): void {
   // assuming transaction.from is the user who burned the tokens
   handleTransferEvents(event, event.transaction.from, Address.zero(), event.params.amount, event.address.toHexString())
+  // handling pool
+  // amount0 is non moxie (for current case), may need to extend in the future
+  // amount1 is moxie (for current case), may need to extend in the future
+  let pool = getOrCreatePoolEntity(event, event.address.toHexString())
+  pool.nonMoxieReserve = pool.nonMoxieReserve.minus(event.params.amount0)
+  pool.moxieReserve = pool.moxieReserve.minus(event.params.amount1)
+  pool.updatedAt = getOrCreateBlockInfo(event).id
+  pool.save()
 }
 
 //When the Aerodrome Gauge Contract is invoked when LP Tokens are deposited we need to track it
