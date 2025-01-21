e3f140d5ba57acdacd809cb638afc7dba8cfdb4f
Sharath V <sharathkrml@gmail.com>
2025-01-21 22:02:39 +0530

    added uniqueHolders fix

--- moxie-protocol-snapshots/src/utils.ts
+++ moxie-protocol-snapshots/src/utils.ts
@@ -2,6 +2,7 @@ import { Address, BigDecimal, BigInt, Bytes, ethereum, log, store, ByteArray, da
 import { ERC20 } from "../generated/TokenManager/ERC20"
 import { BlockInfo, Portfolio, SubjectToken, SubjectTokenDailySnapshot, SubjectTokenHourlySnapshot, Summary, User, SubjectTokenRollingDailySnapshot, Auction, } from "../generated/schema"
 import { BLACKLISTED_AUCTION, BLACKLISTED_SUBJECT_TOKEN_ADDRESS, ONBOARDING_STATUS_ONBOARDING_INITIALIZED, PCT_BASE, SECONDS_IN_DAY, SECONDS_IN_HOUR, SUMMARY_ID, TOKEN_DECIMALS, WHITELISTED_CONTRACTS_MAINNET, WHITELISTED_CONTRACTS_TESTNET } from "./constants"
+import { staking } from "./contracts"
 
 export function getOrCreateSubjectToken(subjectTokenAddress: Address, block: ethereum.Block, saveSubjectTokenFlag: bool = true): SubjectToken {
   let subjectToken = SubjectToken.load(subjectTokenAddress.toHexString())
@@ -51,10 +52,12 @@ export function getOrCreatePortfolio(userAddress: Address, subjectAddress: Addre
   if (!portfolio) {
     portfolio = new Portfolio(portfolioId)
     let subjectToken = getOrCreateSubjectToken(subjectAddress, block, false)
-    // new holder
-    subjectToken.uniqueHolders = subjectToken.uniqueHolders.plus(
-      BigInt.fromI32(1)
-    )
+    if (userAddress != Address.zero() && userAddress.toHexString().toLowerCase() != staking.toLowerCase()) {
+      // new holder
+      subjectToken.uniqueHolders = subjectToken.uniqueHolders.plus(
+        BigInt.fromI32(1)
+      )
+    }
     saveSubjectToken(subjectToken, block)
     portfolio.user = user.id
     portfolio.subjectToken = subjectToken.id
